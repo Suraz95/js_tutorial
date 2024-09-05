d8720acfeeee67963f19ae827f6577bf912e00fa
Suraz95 <shaik.suraz50@gmail.com>
2024-09-05 20:22:55 +0530

    upto arrays

---
+++ arrays.js
@@ -0,0 +1,46 @@
+// let arr = [10, 20, 30, 40, 50];
+//  foreach loop
+//  Map
+//  Filter
+// array.forEach(element => {
+
+// });
+// arr.forEach(function () {
+
+// })
+// arr.forEach(item => {
+//   console.log(item * 5);
+// });
+// arr.forEach((item,index,arr) => {
+//       console.log(item,index,arr);
+    
+// });
+// let newarr = arr.forEach(item => {return item})
+// console.log(newarr)
+// Problem with ForEach Loop is we cant return value
+// arr.map((item,index,arr) => {
+//     console.log(item,index,arr)
+// })
+// chaining in Map
+// let newarr = arr.map(item => item*5)
+//     .map(item => item + 1)
+                
+// console.log(newarr)
+
+// filter
+// let newarr = arr.filter(item => {return item > 20})
+// console.log(newarr)
+// const books = [
+//   { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+//   { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+//   { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+//   { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+//   { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+//   { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+//   { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+//   { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+//   { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+// ];
+
+// let history_books = books.filter(bk => bk.genre === "History" && bk.edition>2000 )
+// console.log(history_books)
---
+++ functions.js
@@ -0,0 +1,62 @@
+//  Functions
+// int functionname(){
+
+// }
+//  JS functions syntax
+// function functionname() {
+
+// }
+// function sayhello() {
+//     console.log("helo Anil")
+// }
+
+//  function with parameters
+
+// function add(num1, num2) {
+//     // console.log(num1+num2)
+//     return num1 + num2;
+//     console.log("hello")
+// }
+// let result1 = add(12, 13)
+// let result2 = add(20, 30)
+// console.log(result1)
+// console.log(result2)
+//  Anonymous function => A function which doesn't have any name
+// let add=function(num1,num2) {
+//     return num1 + num2;
+// }
+// console.log(add(12,14))
+// Arrow functions
+// ()=>{}
+// let add = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(add(12, 14));
+// let add = num => num + 5
+// let add = (num1, num2) => (num1 + num2)
+// console.log(add(12, 14));
+// IIFE - Immediately Invoked function Expression
+// (function () {
+//     return "hello"
+// })()
+// console.log(str)
+// higher order functions -> A function which take another function as its parameter.
+// function add(a, b) {
+//     return a+b
+// }
+// function sub(a, b) {
+//     return a-b
+// }
+// function mul(a, b) {
+//     return a*b
+// }
+// function div(a, b) {
+//     return a/b
+// }
+// function solve(fun, num1, num2) {
+//     return fun(num1,num2)
+// }
+// console.log(solve(add, 12, 13));
+// console.log(solve(sub, 12, 13));
+// console.log(solve(mul, 12, 13));
+// console.log(solve(div, 12, 13));
